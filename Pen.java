57c27f9e15685e32bf2493c00068fa7f4deea518
Edith Stevenson <estevenson23@students.polytechnic.org>
2023-01-30 10:26:54 -0800

    pen - work in progress

--- Pen.java
+++ Pen.java
@@ -1,33 +1,105 @@
 
 /**
- * Write a description of class Pen here.
+ * It's a pen!
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Edith and Grace
+ * @version 01-30-2023
  */
 public class Pen
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private boolean cap;
+    private double inkAmount;
 
     /**
      * Constructor for objects of class Pen
      */
     public Pen()
     {
-        // initialise instance variables
-        x = 0;
+        cap = false;
+        inkAmount = 1.5;
     }
 
     /**
-     * An example of a method - replace this comment with your own
+     * Tells user if cap is on or off
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * @return    cap status (on or off)
      */
-    public int sampleMethod(int y)
+    public boolean getCap()
     {
-        // put your code here
-        return x + y;
+        return cap;
+    }
+    
+    /**
+     * Puts cap on
+     * 
+     */
+    public void capOn()
+    {
+        cap = true;
+    }
+    
+    
+    /**
+     * Puts cap on
+     * 
+     */
+    public void capOff()
+    {
+        cap = true;
+    }
+    
+    /**
+     * Returns amount of ink left
+     *
+     * @return    amount of ink
+     */
+    public double getInkAmount()
+    {
+        return inkAmount;
+    }
+    
+    /**
+     * Refills ink
+     *
+     * @return    amount of ink
+     */
+    public void refillInk()
+    {
+        inkAmount=1.5;
+    }
+    
+    /**
+     * writes message (only if cap is on) and subtracts ink
+     *
+     * @return    amount of ink
+     */
+    public String write(String message)
+    {
+        int characters = 0;
+        if (cap)
+        {
+            for (int i = 0; i < message.length(); i++)
+            {
+                if (!message.substring(i).equals(" "))
+                {
+                    characters += 1;
+                }
+            }
+            
+            for (int j = 0; j<characters; j++)
+            {
+                inkAmount -= 0.001;
+                if (inkAmount <= 0)
+                {
+                    message += " You've ran out of ink";
+                    break;
+                }
+            }
+            return message;
+        }
+        else
+        {
+            return "Whoops! The cap is still on!";
+        }
     }
 }
